4b02c4456ff6ca8ff47bb9ce88e4bfe1c880c4fd
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2023-12-25 15:52:36 -0500

    Pagina para notas agregadas

---
+++ NotePage.xaml.cs
@@ -0,0 +1,29 @@
+namespace Notes;
+
+public partial class NotePage : ContentPage
+{
+    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+
+    public NotePage()
+	{
+		InitializeComponent();
+        if (File.Exists(_fileName))
+            TextEditor.Text = File.ReadAllText(_fileName);
+            Console.WriteLine($"File path: {_fileName}");
+    }
+
+    private void SaveButton_Clicked(object sender, EventArgs e)
+    {
+        // Save the file.
+        File.WriteAllText(_fileName, TextEditor.Text);
+    }
+
+    private void DeleteButton_Clicked(object sender, EventArgs e)
+    {
+        // Delete the file.
+        if (File.Exists(_fileName))
+            File.Delete(_fileName);
+
+        TextEditor.Text = string.Empty;
+    }
+}
