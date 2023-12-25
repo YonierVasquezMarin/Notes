e0bf896f50b883322cfe37e4cdbc5d20b7247bf4
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2023-12-25 17:40:30 -0500

    Se listan varias notas

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -5,6 +5,7 @@
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute(nameof(Views.NotePage), typeof(Views.NotePage));
         }
     }
 }
--- Views/NotePage.xaml.cs
+++ Views/NotePage.xaml.cs
@@ -1,8 +1,12 @@
 namespace Notes.Views;
 
+[QueryProperty(nameof(ItemId), nameof(ItemId))]
 public partial class NotePage : ContentPage
 {
-    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+    public string ItemId
+    {
+        set { LoadNote(value); }
+    }
 
     public NotePage()
 	{
@@ -28,18 +32,23 @@ public partial class NotePage : ContentPage
         BindingContext = noteModel;
     }
 
-    private void SaveButton_Clicked(object sender, EventArgs e)
+    private async void SaveButton_Clicked(object sender, EventArgs e)
     {
-        // Save the file.
-        File.WriteAllText(_fileName, TextEditor.Text);
+        if (BindingContext is Models.Note note)
+            File.WriteAllText(note.Filename, TextEditor.Text);
+
+        await Shell.Current.GoToAsync("..");
     }
 
-    private void DeleteButton_Clicked(object sender, EventArgs e)
+    private async void DeleteButton_Clicked(object sender, EventArgs e)
     {
-        // Delete the file.
-        if (File.Exists(_fileName))
-            File.Delete(_fileName);
+        if (BindingContext is Models.Note note)
+        {
+            // Delete the file.
+            if (File.Exists(note.Filename))
+                File.Delete(note.Filename);
+        }
 
-        TextEditor.Text = string.Empty;
+        await Shell.Current.GoToAsync("..");
     }
 }
