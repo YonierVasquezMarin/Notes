7f68d4684f3c50d4bae84b932b74fdcdd72f6ea9
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2023-12-25 17:05:41 -0500

    Refactor para cargar notas

--- Views/NotePage.xaml.cs
+++ Views/NotePage.xaml.cs
@@ -7,9 +7,25 @@ public partial class NotePage : ContentPage
     public NotePage()
 	{
 		InitializeComponent();
-        if (File.Exists(_fileName))
-            TextEditor.Text = File.ReadAllText(_fileName);
-            Console.WriteLine($"File path: {_fileName}");
+
+        string appDataPath = FileSystem.AppDataDirectory;
+        string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
+
+        LoadNote(Path.Combine(appDataPath, randomFileName));
+    }
+
+    private void LoadNote(string fileName)
+    {
+        Models.Note noteModel = new Models.Note();
+        noteModel.Filename = fileName;
+
+        if (File.Exists(fileName))
+        {
+            noteModel.Date = File.GetCreationTime(fileName);
+            noteModel.Text = File.ReadAllText(fileName);
+        }
+
+        BindingContext = noteModel;
     }
 
     private void SaveButton_Clicked(object sender, EventArgs e)
