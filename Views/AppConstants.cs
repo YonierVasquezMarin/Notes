47738079c5e707390e30aee4918545ef994e0661
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2023-12-31 12:11:10 -0500

    Constantes compartidas agregadas

---
+++ Views/AppConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Notes.Views
+{
+    internal class AppConstants
+    {
+        public static readonly Color BackgroundColor = Color.FromRgb(58, 153, 215);
+        public static readonly Color SecondColor = Color.FromRgb(58, 153, 215);
+    }
+}
